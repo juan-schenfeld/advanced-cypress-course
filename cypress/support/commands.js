f2ccd11fa568507e183744dc0d13c780124519bd
juan schenfeld <juan-schenfeld-p@outlook.com>
2024-01-11 10:30:29 -0300

    solved a typo in login command 'username selector'

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 Cypress.Commands.add('login', (user = "Admin", pw="admin123") => {
-    cy.get('[type="submit"]').type(user)
+    cy.get('[name="username"]').type(user)
     cy.get('[type="password"]').type(pw)
     cy.get('[type="submit"]').click();
 })
@@ -12,5 +12,14 @@ Cypress.Commands.add('visitHomepage',() => {
         url: '/web/index.php/core/i18n/messages'})
         .as('loadHomePage')
         cy.visit('/')
+        cy.wait('@loadHomePage')
+})
+
+Cypress.Commands.add('mainMenuSelect', itemName => {
+    cy.intercept({
+        method: 'GET',
+        url: '/web/index.php/core/i18n/messages'})
+        .as('loadHomePage')
+        cy.get('li > a').contains(itemName).click()
         cy.wait('@loadHomePage')
 })
